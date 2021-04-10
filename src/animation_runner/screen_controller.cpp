0dbabce82b25074d14bac9572557e86fa7619190
Menno van der Graaf <mennovandergraaf@hotmail.com>
2021-04-10 17:08:22 +0200

    Add LED panel driver

--- src/animation_runner/screen_controller.cpp
+++ src/animation_runner/screen_controller.cpp
@@ -1,6 +1,8 @@
 #include "screen_controller.hpp"
 
-#include <QDebug>
+#include <QLoggingCategory>
+
+#include "ws2811.h"
 
 namespace {
 constexpr int kScreenWidth = 8;
@@ -8,9 +10,40 @@ constexpr int kScreenHeight = 8;
 constexpr int kPixelCount = kScreenWidth * kScreenHeight;
 }  // namespace
 
+Q_LOGGING_CATEGORY(ScreenControllerLog, "animation.screen", QtInfoMsg)
+
+ScreenController::ScreenController()
+    : led_panel_(std::make_unique<ws2811_t>()) {
+  if (const ws2811_return_t status = ws2811_init(led_panel_.get());
+      status != WS2811_SUCCESS) {
+    led_panel_.reset();
+    qCWarning(ScreenControllerLog)
+        << "Driver init:" << ws2811_get_return_t_str(status);
+  }
+}
+
+ScreenController::~ScreenController() { ws2811_fini(led_panel_.get()); }
+
 void ScreenController::draw(const std::vector<QColor>& pixels) {
+  if (!led_panel_) {
+    Q_ASSERT_X(false, Q_FUNC_INFO, "Screen not initialized");
+    return;
+  }
+
+  if (const ws2811_return_t status = ws2811_wait(led_panel_.get());
+      status != WS2811_SUCCESS) {
+    qCWarning(ScreenControllerLog)
+        << "Driver wait:" << ws2811_get_return_t_str(status);
+  }
+
   for (const auto& pixel : pixels) {
-    qDebug() << pixel;
+    qCDebug(ScreenControllerLog) << pixel;
+  }
+
+  if (const ws2811_return_t status = ws2811_render(led_panel_.get());
+      status != WS2811_SUCCESS) {
+    qCWarning(ScreenControllerLog)
+        << "Driver render:" << ws2811_get_return_t_str(status);
   }
 }
 
--- src/animation_runner/screen_controller.hpp
+++ src/animation_runner/screen_controller.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <memory>
+
 #include "screen_controller_interface.hpp"
+
+struct ws2811_t;
 
 class ScreenController final : public ScreenControllerInterface {
  public:
-  virtual ~ScreenController() final = default;
+  ScreenController();
+  virtual ~ScreenController() final;
 
   void draw(const std::vector<QColor>& pixels) final;
   QSize getResolution() final;
+
+ private:
+  std::unique_ptr<ws2811_t> led_panel_;
 };
