608e53caf61066bb4ad8302755fc3fdc33cf5cd1
Menno van der Graaf <mennovandergraaf@hotmail.com>
2021-04-27 00:16:57 +0200

    Add ace code editor

--- website/src/components/CodeEditor.vue
+++ website/src/components/CodeEditor.vue
@@ -12,7 +12,13 @@
           Run in Emulator
         </v-btn>
 
-        <v-textarea solo v-model="text_editor" name="input-7-4" label="Code Editor"></v-textarea>
+
+        <div class="container" style="display: flex; flex: 1;">
+          <editor v-model="content" @init="editorInit" width="500" height="500" lang="javascript" theme="monokai"></editor>
+        </div>
+
+
+        <!-- <v-textarea solo v-model="text_editor" name="input-7-4" label="Code Editor"></v-textarea> -->
       </v-col>
     </v-row>
   </v-container>
@@ -22,17 +28,35 @@
   export default {
     data() {
       return {
-        text_editor: "screen.setPixel(0, 0, 'red');\nscreen.setPixel(2, 5, 'green');\nscreen.setPixel(6, 6, 'blue');\nscreen.draw();",
+        content: "screen.setPixel(0, 0, 'red');\nscreen.setPixel(2, 5, 'green');\nscreen.setPixel(6, 6, 'blue');\nscreen.draw();",
       }
     },
 
+    components: {
+        editor: require('vue2-ace-editor'),
+    },
+
     methods: {
+      editorInit: function (editor) {
+        require('brace/ext/language_tools') //language extension prerequsite...
+        require('brace/mode/javascript')    //language
+        require('brace/mode/less')
+        require('brace/theme/monokai')
+        require('brace/snippets/javascript') //snippet
+        editor.setOptions({
+            maxLines: 30,
+            wrap: true,
+            enableBasicAutocompletion: true,
+            autoScrollEditorIntoView: true
+        });
+      },
+
       save: function () {
         console.log("Save Test Log");
       },
 
       runInEmulator: function () {
-        this.$emit('runInEmulator', this.text_editor)
+        this.$emit('runInEmulator', this.content)
       },
     }
   }
