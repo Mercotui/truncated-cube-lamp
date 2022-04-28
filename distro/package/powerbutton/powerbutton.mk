d753da53b689b8670cd09b87d2c91f89ff2148d1
Menno van der Graaf <mennovandergraaf@hotmail.com>
2022-04-29 01:01:40 +0200

    Add powerbutton service to buildroot

---
+++ distro/package/powerbutton/powerbutton.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# powerbutton
+#
+################################################################################
+
+POWERBUTTON_VERSION = 1.0
+POWERBUTTON_LICENSE = MIT
+POWERBUTTON_SITE = $(BR2_EXTERNAL_TRUNCATED_CUBE_LINUX_PATH)/../powerbutton
+POWERBUTTON_SITE_METHOD = local
+
+define POWERBUTTON_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/powerbutton.py $(TARGET_DIR)/usr/local/bin/
+    $(INSTALL) -D -m 0644 $(@D)/powerbutton.service $(TARGET_DIR)/etc/systemd/system/
+endef
+
+$(eval $(generic-package))
